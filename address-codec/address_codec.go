977e87e7a895a6b0a4504553a7dc4a3934494493
HolaVencH <joshua.antonio.long@gmail.com>
2022-05-26 19:58:22 +0100

    CHORE: Encode address from pub key

--- address-codec/address_codec.go
+++ address-codec/address_codec.go
@@ -3,7 +3,6 @@ package addresscodec
 import (
 	"crypto/sha256"
 	"encoding/hex"
-	"errors"
 	"fmt"
 
 	"golang.org/x/crypto/ripemd160"
@@ -15,18 +14,26 @@ const (
 	FamilySeedLength       = 16
 	NodePublicKeyLength    = 33
 
-	ED25519PrefixHex = "ED"
-	pubkeyHex        = "9434799226374926EDA3B54B1B461B4ABF7237962EAE18528FEA67595397FA32" //just an example
+	AccountAddressPrefix   = 0x00
+	AccountPublicKeyPrefix = 0x23
+	FamilySeedPrefix       = 0x21
+	NodePublicKeyPrefix    = 0x1C
+
+	ED25519Prefix          = 0xED
+	ED25519PrefixHexString = "ED" //hex prefix for 32 byte ED25519 hex strings, to make length 33 bytes after decoding
 )
 
-var (
-	AccountAddressPrefix   = []byte{0x00}
-	AccountPublicKeyPrefix = []byte{0x23}
-	FamilySeedPrefix       = []byte{0x21}
-	NodePublicKeyPrefix    = []byte{0x1C}
+type EncodeLengthError struct {
+	Instance string
+	Input    int
+}
 
-	pubkey, _        = hex.DecodeString(pubkeyHex)
-	ED25519Prefix, _ = hex.DecodeString(ED25519PrefixHex)
+func (e *EncodeLengthError) Error() string {
+	return fmt.Sprintf("%v length should be %v", e.Instance, e.Input)
+}
+
+var (
+	ED25519PrefixByteSlice, _ = hex.DecodeString(ED25519PrefixHexString)
 )
 
 func Sha256RipeMD160(b []byte) []byte {
@@ -39,7 +46,7 @@ func Sha256RipeMD160(b []byte) []byte {
 	return ripemd160.Sum(nil)
 }
 
-func Sha256Sha256(b []byte) []byte {
+func CreateCheckSum(b []byte) []byte {
 	sha256 := sha256.New()
 	sha256.Write(b)
 
@@ -50,35 +57,37 @@ func Sha256Sha256(b []byte) []byte {
 	return sha256.Sum(nil)
 }
 
-func EncodeAddress(pubkeyhex string) (string, error) {
+func EncodeAddressFromPublicKeyHex(pubkeyhex string, typePrefix []byte) (string, error) {
 
-	prefixandpubkey := append(ED25519Prefix, pubkey...)
+	pubkey, _ := hex.DecodeString(pubkeyhex)
 
-	if len(prefixandpubkey) != AccountPublicKeyLength {
-		return "", errors.New("public key length should be 33")
+	if len(pubkey) != AccountPublicKeyLength {
+		return "", &EncodeLengthError{Instance: "PublicKey", Input: AccountPublicKeyLength}
 	}
 
-	accountID := Sha256RipeMD160(prefixandpubkey)
+	accountID := Sha256RipeMD160(pubkey)
 
 	if len(accountID) != AccountAddressLength {
-		return "", errors.New("account ID length should be 20")
+		return "", &EncodeLengthError{Instance: "AccountID", Input: AccountAddressLength}
 	}
 
-	payload := append(AccountAddressPrefix, accountID...)
+	payload := append(typePrefix, accountID...)
 
 	if len(payload) != 21 {
-		return "", errors.New("payload length should be 21")
+		return "", &EncodeLengthError{Instance: "Payload", Input: 21}
 	}
 
-	checkSum := Sha256Sha256(accountID)[0:4]
+	checkSum := CreateCheckSum(payload)[:4]
 
 	if len(checkSum) != 4 {
-		return "", errors.New("checksum length should be 4")
+		return "", &EncodeLengthError{Instance: "CheckSum", Input: 4}
 	}
 
 	address := EncodeBase58((append(payload, checkSum...)))
 
-	fmt.Println(address)
+	if len(address) != 34 { //can they be different lengths?
+		return "", &EncodeLengthError{Instance: "Address", Input: 34}
+	}
 
-	return "rDTXLQ7ZKZVKz33zJbHjgVShjsBnqMBhmN", nil
+	return address, nil
 }
--- address-codec/address_codec_test.go
+++ address-codec/address_codec_test.go
@@ -1,33 +1,48 @@
 package addresscodec
 
 import (
-	"encoding/hex"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEncodeAddress(t *testing.T) {
+func TestEncodeAddressFromPublicKeyHex(t *testing.T) {
 	tt := []struct {
-		description string
-		input       string
-		output      string
+		description   string
+		input         string
+		prefix        []byte
+		output        string
+		expectedError error
 	}{
 		{
-			description: "test 1",
-			input:       "ED9434799226374926EDA3B54B1B461B4ABF7237962EAE18528FEA67595397FA32",
-			output:      "rDTXLQ7ZKZVKz33zJbHjgVShjsBnqMBhmN",
+			description:   "Successfully generate address from public key hex string",
+			input:         "ED9434799226374926EDA3B54B1B461B4ABF7237962EAE18528FEA67595397FA32",
+			prefix:        []byte{AccountAddressPrefix},
+			output:        "rDTXLQ7ZKZVKz33zJbHjgVShjsBnqMBhmN",
+			expectedError: nil,
 		},
+		{
+			description:   "Invalid Public Key",
+			input:         "yurt",
+			prefix:        []byte{AccountAddressPrefix},
+			output:        "",
+			expectedError: &EncodeLengthError{Instance: "PublicKey", Input: AccountPublicKeyLength},
+		},
+		// More test cases
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.description, func(t *testing.T) {
 
-			got, _ := EncodeAddress(tc.input)
-			assert.Equal(t, tc.output, got)
-			assert.Equal(t, pubkeyHex, strings.ToUpper(hex.EncodeToString(pubkey)))
-			assert.Equal(t, ED25519PrefixHex, strings.ToUpper(hex.EncodeToString(ED25519Prefix)))
+			got, err := EncodeAddressFromPublicKeyHex(tc.input, tc.prefix)
+
+			if tc.expectedError != nil {
+				assert.Error(t, tc.expectedError, err.Error())
+			} else {
+				if assert.NoError(t, tc.expectedError, err) {
+					assert.Equal(t, tc.output, got)
+				}
+			}
 		})
 	}
 }
