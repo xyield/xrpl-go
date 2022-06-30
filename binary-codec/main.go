d582b1620577150bf07da91bb90838c240d631d6
HolaVencH <joshua.antonio.long@gmail.com>
2022-06-30 11:08:38 +0100

    serialize Flags

--- binary-codec/main.go
+++ binary-codec/main.go
@@ -1,28 +1,74 @@
 package binarycodec
 
 import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
 	"sort"
 
 	"github.com/xyield/xrpl-go/binary-codec/definitions"
 )
 
-func Encode(json map[string]interface{}) (string, error) {
+func Encode(json map[string]any) (string, error) {
+
+	fimap, err := createFieldInstanceMapFromJson(json)
+
+	if err != nil {
+		return "", err
+	}
+
+	sk := getSortedKeys(fimap)
+
+	var sink []byte
+
+	for _, v := range sk {
+
+		h, err := EncodeFieldID(v.FieldName)
+
+		if err != nil {
+			return "", err
+		}
+
+		sink = append(sink, h...)
+		fmt.Println(hex.EncodeToString(sink))
+
+		// fmt.Println(fimap[v])
+		// val := uint32(fimap[v].(int))
+
+		// need to write bytes to new buffers
+		// amount, uint, hash all big endian
+		buf := new(bytes.Buffer)
+		err = binary.Write(buf, binary.BigEndian, uint32(fimap[v].(int)))
+
+		if err != nil {
+			return "", err
+		}
+
+		fmt.Println(buf.Bytes())
+		sink = append(sink, buf.Bytes()...)
+		// fmt.Println(hex.EncodeToString(sink))
+	}
 
 	// Loop through and create map of map[FieldInstance]interface{}
 	// Sort by Ordinal
 	// Start serializing
-	return "", nil
+	//	optimize encode from field id codec, making same call twice
+
+	// fmt.Println(string(sink))
+
+	return hex.EncodeToString(sink), nil
 }
 
-func Serialize(json string) (string, error) {
-	return "120007220008000024001ABED82A2380BF2C2019001ABED764D55920AC9391400000000000000000000000000055534400000000000A20B3C85F482532A9578DBB3950B85CA06594D165400000037E11D60068400000000000000A732103EE83BB432547885C219634A1BC407A9DB0474145D69737D09CCDC63E1DEE7FE3744630440220143759437C04F7B61F012563AFE90D8DAFC46E86035E1D965A9CED282C97D4CE02204CFD241E86F17E011298FC1A39B63386C74306A5DE047E213B0F29EFA4571C2C8114DD76483FACDEE26E60D8A586BB58D09F27045C46", nil
-}
+// func Serialize(json string) (string, error) {
+// 	return "120007220008000024001ABED82A2380BF2C2019001ABED764D55920AC9391400000000000000000000000000055534400000000000A20B3C85F482532A9578DBB3950B85CA06594D165400000037E11D60068400000000000000A732103EE83BB432547885C219634A1BC407A9DB0474145D69737D09CCDC63E1DEE7FE3744630440220143759437C04F7B61F012563AFE90D8DAFC46E86035E1D965A9CED282C97D4CE02204CFD241E86F17E011298FC1A39B63386C74306A5DE047E213B0F29EFA4571C2C8114DD76483FACDEE26E60D8A586BB58D09F27045C46", nil
+// }
 
 //lint:ignore U1000 // ignore this for now
 //nolint
-func createFieldInstanceMapFromJson(json map[string]interface{}) (map[definitions.FieldInstance]interface{}, error) {
+func createFieldInstanceMapFromJson(json map[string]any) (map[definitions.FieldInstance]any, error) {
 
-	m := make(map[definitions.FieldInstance]interface{}, len(json))
+	m := make(map[definitions.FieldInstance]any, len(json))
 
 	for k, v := range json {
 		fi, err := definitions.Get().GetFieldInstanceByFieldName(k)
@@ -40,7 +86,7 @@ func createFieldInstanceMapFromJson(json map[string]interface{}) (map[definition
 
 //lint:ignore U1000 // ignore this for now
 //nolint
-func getSortedKeys(m map[definitions.FieldInstance]interface{}) []definitions.FieldInstance {
+func getSortedKeys(m map[definitions.FieldInstance]any) []definitions.FieldInstance {
 	keys := make([]definitions.FieldInstance, 0, len(m))
 
 	for k := range m {
--- binary-codec/main_test.go
+++ binary-codec/main_test.go
@@ -109,46 +109,53 @@ func TestGetSortedKeys(t *testing.T) {
 		})
 	}
 }
-func TestSerialize(t *testing.T) {
+func TestEncode(t *testing.T) {
 	tt := []struct {
 		description string
-		input       string
+		input       map[string]any
 		output      string
 		expectedErr error
 	}{
+		// {
+		// 	description: "successfullty serialized signed transaction",
+		// 	input: `{
+		// 		"Account": "rMBzp8CgpE441cp5PVyA9rpVV7oT8hP3ys",
+		// 		"Expiration": 595640108,
+		// 		"Fee": "10",
+		// 		"Flags": 524288,
+		// 		"OfferSequence": 1752791,
+		// 		"Sequence": 1752792,
+		// 		"SigningPubKey": "03EE83BB432547885C219634A1BC407A9DB0474145D69737D09CCDC63E1DEE7FE3",
+		// 		"TakerGets": "15000000000",
+		// 		"TakerPays": {
+		// 		  "currency": "USD",
+		// 		  "issuer": "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+		// 		  "value": "7072.8"
+		// 		},
+		// 		"TransactionType": "OfferCreate",
+		// 		"TxnSignature": "30440220143759437C04F7B61F012563AFE90D8DAFC46E86035E1D965A9CED282C97D4CE02204CFD241E86F17E011298FC1A39B63386C74306A5DE047E213B0F29EFA4571C2C",
+		// 		"hash": "73734B611DDA23D3F5F62E20A173B78AB8406AC5015094DA53F53D39B9EDB06C"
+		// 	  }"`,
+		// 	output:      "120007220008000024001ABED82A2380BF2C2019001ABED764D55920AC9391400000000000000000000000000055534400000000000A20B3C85F482532A9578DBB3950B85CA06594D165400000037E11D60068400000000000000A732103EE83BB432547885C219634A1BC407A9DB0474145D69737D09CCDC63E1DEE7FE3744630440220143759437C04F7B61F012563AFE90D8DAFC46E86035E1D965A9CED282C97D4CE02204CFD241E86F17E011298FC1A39B63386C74306A5DE047E213B0F29EFA4571C2C8114DD76483FACDEE26E60D8A586BB58D09F27045C46",
+		// 	expectedErr: nil,
+		// },
 		{
-			description: "successfullty serialized signed transaction",
-			input: `{
-				"Account": "rMBzp8CgpE441cp5PVyA9rpVV7oT8hP3ys",
-				"Expiration": 595640108,
-				"Fee": "10",
-				"Flags": 524288,
-				"OfferSequence": 1752791,
-				"Sequence": 1752792,
-				"SigningPubKey": "03EE83BB432547885C219634A1BC407A9DB0474145D69737D09CCDC63E1DEE7FE3",
-				"TakerGets": "15000000000",
-				"TakerPays": {
-				  "currency": "USD",
-				  "issuer": "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
-				  "value": "7072.8"
-				},
-				"TransactionType": "OfferCreate",
-				"TxnSignature": "30440220143759437C04F7B61F012563AFE90D8DAFC46E86035E1D965A9CED282C97D4CE02204CFD241E86F17E011298FC1A39B63386C74306A5DE047E213B0F29EFA4571C2C",
-				"hash": "73734B611DDA23D3F5F62E20A173B78AB8406AC5015094DA53F53D39B9EDB06C"
-			  }"`,
-			output:      "120007220008000024001ABED82A2380BF2C2019001ABED764D55920AC9391400000000000000000000000000055534400000000000A20B3C85F482532A9578DBB3950B85CA06594D165400000037E11D60068400000000000000A732103EE83BB432547885C219634A1BC407A9DB0474145D69737D09CCDC63E1DEE7FE3744630440220143759437C04F7B61F012563AFE90D8DAFC46E86035E1D965A9CED282C97D4CE02204CFD241E86F17E011298FC1A39B63386C74306A5DE047E213B0F29EFA4571C2C8114DD76483FACDEE26E60D8A586BB58D09F27045C46",
+			description: "test Flags",
+			input:       map[string]any{"Flags": 524288},
+			output:      "2200080000",
 			expectedErr: nil,
 		},
 	}
 
 	for _, tc := range tt {
 		t.Run(tc.description, func(t *testing.T) {
-			got, err := Serialize(tc.input)
+			got, err := Encode(tc.input)
 
 			if tc.expectedErr != nil {
-				assert.Error(t, err, tc.expectedErr.Error())
+				assert.EqualError(t, err, tc.expectedErr.Error())
 				assert.Empty(t, got)
 			} else {
+				assert.NoError(t, err)
 				assert.Equal(t, tc.output, got)
 			}
 		})
