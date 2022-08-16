43475f86f7e155dae32d912f43b581646261e54a
HolaVencH <joshua.antonio.long@gmail.com>
2022-08-16 14:35:52 +0100

    UInt16 Serialized

--- binary-codec/types/uint16.go
+++ binary-codec/types/uint16.go
@@ -3,14 +3,30 @@ package types
 import (
 	"bytes"
 	"encoding/binary"
+
+	"github.com/xyield/xrpl-go/binary-codec/definitions"
 )
 
 type UInt16 struct{}
 
 func (u *UInt16) SerializeJson(value any) ([]byte, error) {
 
+	if _, ok := value.(string); ok {
+		tc, err := definitions.Get().GetTransactionTypeCodeByTransactionTypeName(value.(string))
+
+		if err != nil {
+			tc, err = definitions.Get().GetLedgerEntryTypeCodeByLedgerEntryTypeName(value.(string))
+
+			if err == nil {
+				value = tc
+			}
+		}
+
+		value = tc
+	}
+
 	buf := new(bytes.Buffer)
-	err := binary.Write(buf, binary.BigEndian, uint16(7))
+	err := binary.Write(buf, binary.BigEndian, uint16(value.(int32)))
 
 	if err != nil {
 		return nil, err
