51c86777c87a207da0b0a2dff4639034f30ebe55
HolaVencH <joshua.antonio.long@gmail.com>
2022-08-17 17:14:12 +0100

    added comment to explain 0x14 byte prefix for AccountID

--- binary-codec/types/account_id.go
+++ binary-codec/types/account_id.go
@@ -13,5 +13,12 @@ func (a *AccountID) SerializeJson(value any) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	//AccountIDs that appear as stand-alone fields (such as Account and Destination)
+	// are length-prefixed despite being a fixed 160 bits in length. As a result,
+	// the length indicator for these fields is always the byte 0x14.
+	//
+	// AccountIDs that appear as children of special fields (Amount issuer and PathSet account) are not length-prefixed.
+
 	return append([]byte{0x14}, accountID...), nil
 }
