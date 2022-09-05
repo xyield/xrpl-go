0c599578636454568c3aa0e1815497c0bb80e500
HolaVencH <joshua.antonio.long@gmail.com>
2022-09-05 17:18:34 +0100

    added more funcs to Amount serialized type

--- binary-codec/types/amount.go
+++ binary-codec/types/amount.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"errors"
+	"fmt"
 	"math/big"
 	"strings"
 )
@@ -43,7 +44,7 @@ func VerifyXrpValue(value string) error {
 	}
 
 	decimal := new(big.Float)
-	decimal, ok := decimal.SetString(value) // may need to be bigFloat for precision
+	decimal, ok := decimal.SetString(value) // bigFloat for precision
 
 	if !ok {
 		return errors.New("failed to convert string to big.Float")
@@ -63,9 +64,7 @@ func VerifyXrpValue(value string) error {
 // validates the format of an issued currency amount
 func VerifyIOUValue(value string) error {
 
-	decimalValue := new(big.Float)
-
-	decimalValue, ok := decimalValue.SetString(value)
+	decimalValue, ok := new(big.Float).SetString(value) // bigFloat for precision
 
 	if !ok {
 		return errors.New("failed to convert string to big.Float")
@@ -75,6 +74,52 @@ func VerifyIOUValue(value string) error {
 		return nil
 	}
 
-	// var exponent = decimalValue.MantExp(nil)
+	fmt.Println("decimalValue raw:", decimalValue)
+	precision := decimalValue.MinPrec()
+	fmt.Println("precision raw", precision)
+	exponent := decimalValue.MantExp(decimalValue)
+
+	fmt.Println("decimalValue after:", decimalValue)
+	fmt.Println("precision after:", precision)
+
+	// Exponent must be between -96 and 80 - need further validation
+	if precision > MAX_IOU_PRECISION || exponent < MIN_IOU_EXPONENT || exponent > MAX_IOU_EXPONENT {
+		return errors.New("IOU value is an invalid IOU amount - precision is too large > 16")
+	}
+
+	// verify there is no decimal point after being multiplied by exponent
+	if !containsDecimal(fmt.Sprint(exponent)) {
+		return nil
+	} else {
+		return errors.New("IOU value is an invalid IOU amount - contains a decimal point")
+	}
+
+}
+
+// Serializes the value field of an issued currency amount to its bytes representation
+func SerializeIssuedCurrencyValue(value string) []byte {
+
+	VerifyIOUValue(value)
+
+	decimalValue, _ := new(big.Float).SetString(value) // bigFloat for precision
+	if decimalValue.Sign() == 0 {
+		x := new(big.Int).SetUint64(ZERO_CURRENCY_AMOUNT_HEX)
+		fmt.Println("x:", x)
+		return []byte(x.Bytes())
+	}
+
+	// convert components to integers
+
 	return nil
+}
+
+// Returns true is this amount is a "native" XRP amount
+func IsNative(value []byte) bool {
+	return []byte(value)[0]&0x80 == 0
+}
+
+// Determines if this AmountType is positive
+func IsPositive(value []byte) bool {
+	// 2nd bit in 1st byte is set to 1 for positive amounts
+	return []byte(value)[0]&0x40 > 0
 }
--- binary-codec/types/amount_test.go
+++ binary-codec/types/amount_test.go
@@ -37,24 +37,24 @@ func TestContainsDecimal(t *testing.T) {
 func TestVerifyXrpValue(t *testing.T) {
 
 	tests := []struct {
-		name  string
-		input string
-		err   error
+		name   string
+		input  string
+		ExpErr error
 	}{
 		{
-			name:  "valid xrp value",
-			input: "1.0",
-			err:   nil,
+			name:   "valid xrp value",
+			input:  "1.0",
+			ExpErr: nil,
 		},
 		{
-			name:  "invalid xrp value - out of range",
-			input: "0.000000007",
-			err:   errors.New("XRP value is an invalid XRP amount"),
+			name:   "invalid xrp value - out of range",
+			input:  "0.000000007",
+			ExpErr: errors.New("XRP value is an invalid XRP amount"),
 		},
 		{
-			name:  "invalid xrp value - no decimal",
-			input: "1",
-			err:   errors.New("XRP value must contain a decimal"),
+			name:   "invalid xrp value - no decimal",
+			input:  "1",
+			ExpErr: errors.New("XRP value must contain a decimal"),
 		},
 	}
 	for _, tt := range tests {
@@ -62,12 +62,11 @@ func TestVerifyXrpValue(t *testing.T) {
 
 			err := VerifyXrpValue(tt.input)
 
-			if err != nil {
-				assert.Equal(t, tt.err.Error(), err.Error())
+			if tt.ExpErr != nil {
+				assert.Equal(t, tt.ExpErr, err)
 			} else {
-				assert.Nil(t, tt.err)
+				assert.Nil(t, err)
 			}
-
 		})
 	}
 }
@@ -75,14 +74,29 @@ func TestVerifyXrpValue(t *testing.T) {
 func TestVerifyIOUValue(t *testing.T) {
 
 	tests := []struct {
-		name  string
-		input string
-		err   error
+		name   string
+		input  string
+		ExpErr error
 	}{
 		{
-			name:  "valid iou value",
-			input: "1.0",
-			err:   nil,
+			name:   "valid iou value",
+			input:  "3.0",
+			ExpErr: nil,
+		},
+		{
+			name:   "invalid iou value - out of range precision",
+			input:  "0.000000000000000000007",
+			ExpErr: errors.New("IOU value is an invalid IOU amount - precision is too large > 16"),
+		},
+		{
+			name:   "invalid iou value - out of range exponent too large",
+			input:  "998000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+			ExpErr: errors.New("IOU value is an invalid IOU amount - precision is too large > 16"),
+		},
+		{
+			name:   "invalid iou value - out of range exponent too small",
+			input:  "0.998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948998456890560948",
+			ExpErr: errors.New("IOU value is an invalid IOU amount - precision is too large > 16"),
 		},
 	}
 	for _, tt := range tests {
@@ -90,11 +104,39 @@ func TestVerifyIOUValue(t *testing.T) {
 
 			err := VerifyIOUValue(tt.input)
 
-			if err != nil {
-				assert.Equal(t, tt.err.Error(), err.Error())
+			if tt.ExpErr != nil {
+				assert.Equal(t, tt.ExpErr, err)
 			} else {
-				assert.Nil(t, tt.err)
+				assert.Nil(t, err)
 			}
+
+		})
+	}
+}
+
+func TestSerializeIssuedCurrencyValue(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected []byte
+	}{
+		{
+			name:     "valid zero value",
+			input:    "0",
+			expected: []byte{0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+		},
+		{
+			name:     "valid value",
+			input:    "3.0567",
+			expected: []byte{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			got := SerializeIssuedCurrencyValue(tt.input)
+
+			assert.Equal(t, tt.expected, got)
 
 		})
 	}
