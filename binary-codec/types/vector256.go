9788479aa98f1a00e2b2ae237e110048cf487ab7
HolaVencH <joshua.antonio.long@gmail.com>
2022-10-18 19:58:01 +0100

    chore: created vector256 type with []byte sig

--- binary-codec/types/vector256.go
+++ binary-codec/types/vector256.go
@@ -12,9 +12,7 @@ func (e *ErrInvalidVector256Type) Error() string {
 	return fmt.Sprintf("Invalid type to construct Vector256 from. Expected []string, got %v", e.Got)
 }
 
-type Vector256 struct {
-	byteArray []byte
-}
+type Vector256 struct{}
 
 func (v *Vector256) SerializeJson(json any) ([]byte, error) {
 
@@ -22,22 +20,19 @@ func (v *Vector256) SerializeJson(json any) ([]byte, error) {
 		return nil, &ErrInvalidVector256Type{fmt.Sprintf("%T", json)}
 	}
 
-	v, err := vector256FromValue(json.([]string))
+	b, err := vector256FromValue(json.([]string))
 
 	if err != nil {
 		return nil, err
 	}
 
-	return v.byteArray, nil
+	return b, nil
 
 }
 
-func vector256FromValue(value []string) (*Vector256, error) {
-
+func vector256FromValue(value []string) ([]byte, error) {
 	b := make([]byte, 0)
-
 	for _, s := range value {
-
 		hash256, err := NewHash256().SerializeJson(s)
 
 		if err != nil {
@@ -47,8 +42,5 @@ func vector256FromValue(value []string) (*Vector256, error) {
 		b = append(b, hash256...)
 
 	}
-
-	return &Vector256{
-		byteArray: b,
-	}, nil
+	return b, nil
 }
