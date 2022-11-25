203c629f0586fe5ca3eaca79754da5d66eb6fc60
HolaVencH <joshua.antonio.long@gmail.com>
2022-11-25 11:15:09 +0000

    chore: encode for multisigning

--- binary-codec/main.go
+++ binary-codec/main.go
@@ -4,7 +4,12 @@ import (
 	"encoding/hex"
 	"strings"
 
+	"github.com/xyield/xrpl-go/binary-codec/definitions"
 	"github.com/xyield/xrpl-go/binary-codec/types"
+)
+
+const (
+	txMultiSigPrefix = "534D5400"
 )
 
 // Encode: encodes a transaction or other object from json to the canonical binary format as a hex string.
@@ -17,4 +22,43 @@ func Encode(json map[string]any) (string, error) {
 	}
 
 	return strings.ToUpper(hex.EncodeToString(b)), nil
+}
+
+// EncodeForMultiSign: encodes a transaction into binary format in preparation for providing one
+// signature towards a multi-signed transaction.
+// (Only encodes fields that are intended to be signed.)
+func EncodeForMultisigning(json map[string]any, xrpAccountID map[string]any) (string, error) {
+
+	st := &types.STObject{}
+
+	// remove the SigningPubKey field because any existing signing keys
+	// shouldn't be signed over again.
+
+	delete(json, "SigningPubKey")
+
+	suffix, err := st.SerializeJson(xrpAccountID)
+	if err != nil {
+		return "", err
+	}
+
+	encoded, err := Encode(removeNonSigningFields(json))
+
+	if err != nil {
+		return "", err
+	}
+
+	return strings.ToUpper(txMultiSigPrefix + encoded + hex.EncodeToString(suffix)), nil
+}
+
+func removeNonSigningFields(json map[string]any) map[string]any {
+
+	for k := range json {
+		fi, _ := definitions.Get().GetFieldInstanceByFieldName(k)
+
+		if fi != nil && !fi.IsSigningField {
+			delete(json, k)
+		}
+	}
+
+	return json
 }
--- binary-codec/main_test.go
+++ binary-codec/main_test.go
@@ -239,3 +239,63 @@ func TestEncode(t *testing.T) {
 	}
 
 }
+
+func TestEncodeForMultisigning(t *testing.T) {
+	tt := []struct {
+		description string
+		json        map[string]any
+		accountID   map[string]any
+		output      string
+		expectedErr error
+	}{
+		{
+			description: "serialize STObject for signing correctly",
+			json: map[string]any{
+				"Memo": map[string]any{
+					"MemoType": "04C4D46544659A2D58525043686174",
+				},
+			},
+			accountID:   map[string]any{"Account": "rMBzp8CgpE441cp5PVyA9rpVV7oT8hP3ys"},
+			output:      "534D5400EA7C0F04C4D46544659A2D58525043686174E18114DD76483FACDEE26E60D8A586BB58D09F27045C46",
+			expectedErr: nil,
+		},
+		{
+			description: "serialize tx1 for signing correctly",
+			json: map[string]any{
+				"Account":       "rMBzp8CgpE441cp5PVyA9rpVV7oT8hP3ys",
+				"Expiration":    595640108,
+				"Fee":           "10",
+				"Flags":         524288,
+				"OfferSequence": 1752791,
+				"Sequence":      1752792,
+				"SigningPubKey": "03EE83BB432547885C219634A1BC407A9DB0474145D69737D09CCDC63E1DEE7FE3",
+				"TakerGets":     "15000000000",
+				"TakerPays": map[string]any{
+					"currency": "USD",
+					"issuer":   "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+					"value":    "7072.8",
+				},
+				"TransactionType": "OfferCreate",
+				"TxnSignature":    "30440220143759437C04F7B61F012563AFE90D8DAFC46E86035E1D965A9CED282C97D4CE02204CFD241E86F17E011298FC1A39B63386C74306A5DE047E213B0F29EFA4571C2C",
+				"hash":            "73734B611DDA23D3F5F62E20A173B78AB8406AC5015094DA53F53D39B9EDB06C",
+			},
+			accountID:   map[string]any{"Account": "rMBzp8CgpE441cp5PVyA9rpVV7oT8hP3ys"},
+			output:      "534D5400120007220008000024001ABED82A2380BF2C2019001ABED764D55920AC9391400000000000000000000000000055534400000000000A20B3C85F482532A9578DBB3950B85CA06594D165400000037E11D60068400000000000000A8114DD76483FACDEE26E60D8A586BB58D09F27045C468114DD76483FACDEE26E60D8A586BB58D09F27045C46",
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.description, func(t *testing.T) {
+			got, err := EncodeForMultisigning(tc.json, tc.accountID)
+
+			if tc.expectedErr != nil {
+				require.EqualError(t, err, tc.expectedErr.Error())
+				require.Empty(t, got)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.output, got)
+			}
+		})
+	}
+}
