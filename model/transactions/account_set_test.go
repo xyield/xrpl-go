5d0c83f008f7e186f67c9e3e8c4596cc15b95109
PR <creature@localhost.localdomain>
2023-02-17 13:05:44 -0800

    tx tests

---
+++ model/transactions/account_set_test.go
@@ -0,0 +1,47 @@
+package transactions
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/xyield/xrpl-go/model/transactions/types"
+	"github.com/xyield/xrpl-go/test"
+)
+
+func TestAccountSetTransaction(t *testing.T) {
+	s := AccountSet{
+		BaseTx: BaseTx{
+			Account:         "abcdef",
+			TransactionType: AccountSetTx,
+			Fee:             types.XRPCurrencyAmount(1),
+			Sequence:        1234,
+			SigningPubKey:   "ghijk",
+			TxnSignature:    "A1B2C3D4E5F6",
+		},
+		Domain:  "A5B21758D2318FA2C",
+		SetFlag: 10,
+	}
+	j := `{
+	"Account": "abcdef",
+	"TransactionType": "AccountSet",
+	"Fee": "1",
+	"Sequence": 1234,
+	"SigningPubKey": "ghijk",
+	"TxnSignature": "A1B2C3D4E5F6",
+	"Domain": "A5B21758D2318FA2C",
+	"SetFlag": 10
+}`
+
+	if err := test.SerializeAndDeserialize(s, j); err != nil {
+		t.Error(err)
+	}
+
+	tx, err := UnmarshalTx(json.RawMessage(j))
+	if err != nil {
+		t.Errorf("UnmarshalTx error: %s", err.Error())
+	}
+	if !reflect.DeepEqual(tx, &s) {
+		t.Error("UnmarshalTx result differs from expected")
+	}
+}
