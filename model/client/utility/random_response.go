516339ed194f48658136e367415730e9db1eee04
PR <creature@localhost.localdomain>
2023-04-07 12:52:10 -0700

    random test

--- model/client/utility/random_response.go
+++ model/client/utility/random_response.go
@@ -1,6 +1,6 @@
 package utility
 
-import "github.com/xyield/xrpl-go/binary-codec/types"
+import "github.com/xyield/xrpl-go/model/transactions/types"
 
 type RandomResponse struct {
 	Random types.Hash256 `json:"random"`
