344df3e8681f9addc575431ca529911000c4d21f
PR <creature@localhost.localdomain>
2023-05-15 16:38:22 -0700

    tests

---
+++ model/client/admin/data/ledger_cleaner_test.go
@@ -0,0 +1,41 @@
+package data
+
+import (
+	"testing"
+
+	"github.com/xyield/xrpl-go/test"
+)
+
+func TestLedgerCleanerRequest(t *testing.T) {
+	s := LedgerCleanerRequest{
+		Ledger:    7,
+		MaxLedger: 5,
+		MinLedger: 3,
+		Stop:      true,
+	}
+
+	j := `{
+	"ledger": 7,
+	"max_ledger": 5,
+	"min_ledger": 3,
+	"stop": true
+}`
+
+	if err := test.SerializeAndDeserialize(t, s, j); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestLedgerCleanerResponse(t *testing.T) {
+	s := LedgerCleanerResponse{
+		Message: "Cleaner configured",
+	}
+
+	j := `{
+	"message": "Cleaner configured"
+}`
+
+	if err := test.SerializeAndDeserialize(t, s, j); err != nil {
+		t.Error(err)
+	}
+}
--- model/client/admin/data/ledger_request_response.go
+++ model/client/admin/data/ledger_request_response.go
@@ -1,20 +1,70 @@
 package data
 
 import (
+	"encoding/json"
+
 	"github.com/xyield/xrpl-go/model/client/common"
 	"github.com/xyield/xrpl-go/model/client/ledger"
 )
 
 type LedgerRequestResponse struct {
-	ledger.LedgerHeader
-	LedgerRequest
-	Acquiring LedgerRequest `json:"acquiring"`
+	LedgerHeader  *ledger.LedgerHeader `json:"ledger,omitempty"`
+	LedgerRequest *LedgerRequest       `json:"-"`
+	Acquiring     *LedgerRequest       `json:"acquiring,omitempty"`
 }
 
 type LedgerRequest struct {
-	Hash              common.LedgerHash `json:"hash,omitempty"`
-	HaveHeader        bool              `json:"have_header"`
-	HaveState         bool              `json:"have_state,omitempty"`
-	HaveTransactions  bool              `json:"have_transactions,omitempty"`
-	NeededStateHashes []string          `json:"needed_state_hashes,omitempty"`
+	Hash                    common.LedgerHash `json:"hash,omitempty"`
+	HaveHeader              bool              `json:"have_header"`
+	HaveState               bool              `json:"have_state,omitempty"`
+	HaveTransactions        bool              `json:"have_transactions,omitempty"`
+	NeededStateHashes       []string          `json:"needed_state_hashes,omitempty"`
+	NeededTransactionHashes []string          `json:"needed_transaction_hashes,omitempty"`
+	Peers                   int               `json:"peers"`
+	Timeouts                int               `json:"timeouts"`
+}
+
+func (r LedgerRequestResponse) MarshalJSON() ([]byte, error) {
+	type rHelper LedgerRequestResponse
+	data := make(map[string]interface{})
+	h := rHelper(r)
+
+	first, err := json.Marshal(h)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(first, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	if r.LedgerRequest != nil {
+		second, err := json.Marshal(r.LedgerRequest)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(second, &data)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return json.Marshal(data)
+}
+
+func (r *LedgerRequestResponse) UnmarshalJSON(data []byte) error {
+	type rHelper LedgerRequestResponse
+	h := rHelper(*r)
+	if err := json.Unmarshal(data, &h); err != nil {
+		return err
+	}
+	r.Acquiring = h.Acquiring
+	r.LedgerHeader = h.LedgerHeader
+	if r.Acquiring == nil && r.LedgerHeader == nil {
+		if err := json.Unmarshal(data, &(r.LedgerRequest)); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
